7575b5684b9cf091be5a4f80cb766a031f2958c6
Yaro Luchko <yaro@mit.edu>
2023-10-17 15:19:22 -0400

    added booster pad in the middle of map

---
+++ Assets/Scripts/Booster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour
+{
+    public LayerMask m_TankMask;                        // Used to filter what the explosion affects, this should be set to "Players".
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        Rigidbody targetRigidbody = collider.GetComponent<Rigidbody>();
+        TankMovement targetMovement = targetRigidbody.GetComponent<TankMovement>();
+        targetMovement.m_Speed = 24f;
+        targetMovement.m_TurnSpeed = 360f;  
+    }
+
+    private void OnTriggerExit(Collider collider) {
+        Rigidbody targetRigidbody = collider.GetComponent<Rigidbody>();
+        TankMovement targetMovement = targetRigidbody.GetComponent<TankMovement>();
+        targetMovement.m_Speed = 12f;
+        targetMovement.m_TurnSpeed = 180f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
