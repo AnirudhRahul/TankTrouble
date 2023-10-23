6a58a7a4877c869556c9e1eea8109a8516d1afb5
Yaro Luchko <yaroslavluchko6@gmail.com>
2023-10-23 01:57:09 -0400

    add alternate controls, limit zooming, swap spawn points, add simple start screen

---
+++ Assets/StartScreenControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenControl : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            // Load the main game scene (replace "MainGame" with the name of your game scene)
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
+
